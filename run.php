2106f3c10cc0581041df088b975e4cb080b30fd7
Andrew Macht <machtap@gmail.com>
2012-10-10 10:10:06 -0500

    4. corret ruby script path

--- run.php
+++ run.php
@@ -5,7 +5,7 @@ $U = $_POST['U'];
 $S = $_POST['S'];
 $D = $_POST['D'];
 
-$string = "./RubyScript giftube" . " '" . $U . "' " . $S . " " . $D;
+$string = "home/ubuntu/RubyScript " . " '" . $U . "' " . $S . " " . $D;
 exec($string);
 
 echo "$string";
