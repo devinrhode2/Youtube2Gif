05c402b6c9afd16187ed8460cefd8c289e2ea949
Andrew Macht <Machtap@gmail.com>
2012-10-10 20:38:16 -0500

    Gif generation works. Fuck ruby.

--- run.php
+++ run.php
@@ -4,10 +4,17 @@ session_start();
 $U = $_POST['U'];
 $S = $_POST['S'];
 $D = $_POST['D'];
+$source_file = '9bZkp7q19f0.mp4';
 
-$string = "home/ubuntu/RubyScript " . " '" . $U . "' " . $S . " " . $D;
+//$download = "youtube-dl " . $U;
+//exec($download);
+
+//$string = "/home/ubuntu/RubyScript " . " '" . $U . "' " . $S . " " . $D;
+// 'mplayer -vo gif89a:15.0:movie.gif file.mpeg'
+$string =  "mplayer 9bZkp7q19f0.mp4 -nosound -vo gif89a:fps=12:output=output.gif -vf scale='550:309' -ss ". $S ." -endpos ". $D;
 exec($string);
 
 echo "$string";
+//echo "$download";
 
 ?>
